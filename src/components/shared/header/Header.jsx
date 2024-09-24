8bc0add50f0cd7053ffc7dd0318f47ef9324999f
KiwiScreams <screamskiwi3@gmail.com>
2024-09-24 19:53:52 +0400

    responsive desktop header

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -4,7 +4,24 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <header>
+      <header className="desktop">
+        <h1 className="pointer">
+          <img src={logo} alt="Skilline" />
+        </h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/">Careers</Link>
+          <Link to="/">Blog</Link>
+          <Link to="/">About Us</Link>
+          <div className="btn-container">
+            <button className="login btn">Login</button>
+            <button to="/" className="signup btn">
+              Sign Us
+            </button>
+          </div>
+        </nav>
+      </header>
+      <header className="mobile">
         <h1 className="pointer">
           <img src={logo} alt="Skilline" />
         </h1>
