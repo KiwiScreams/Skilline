0dc1166d02cffa01eb6c9680557b0eac22c26f58
KiwiScreams <screamskiwi3@gmail.com>
2024-09-24 19:39:00 +0400

    header styles

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -9,12 +9,16 @@ const Header = () => {
           <img src={logo} alt="Skilline" />
         </h1>
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/">Careers</Link>
-            <Link to="/">Blog</Link>
-            <Link to="/">About Us</Link>
-            <Link to="/" className="login btn">Login</Link>
-            <Link to="/" className="signup btn">Sign Up</Link>
+          <Link to="/">Home</Link>
+          <Link to="/">Careers</Link>
+          <Link to="/">Blog</Link>
+          <Link to="/">About Us</Link>
+          <div className="btn-container">
+            <button className="login btn">Login</button>
+            <button to="/" className="signup btn">
+              Sign Us
+            </button>
+          </div>
         </nav>
       </header>
     </>
