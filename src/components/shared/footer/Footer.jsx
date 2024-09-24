d09353f45861bad0c0f6305cf353a7010b924383
KiwiScreams <screamskiwi3@gmail.com>
2024-09-24 22:19:18 +0400

    logo navigate to home

--- src/components/shared/footer/Footer.jsx
+++ src/components/shared/footer/Footer.jsx
@@ -2,6 +2,7 @@ import "./Footer.css";
 import logo from "../../../assets/images/logofooter.svg";
 import { useState } from "react";
 import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 const Footer = () => {
   const [email, setEmail] = useState("");
   const [emailError, setEmailError] = useState(null);
@@ -26,11 +27,15 @@ const Footer = () => {
       }, 50);
     }
   };
+  const navigate = useNavigate();
+  const handleNavigateToHomePage = () => {
+    navigate("/");
+  };
   return (
     <>
       <footer>
         <div className="footer-top-container">
-          <img src={logo} alt="Skilline" />
+          <img src={logo} alt="Skilline" onClick={handleNavigateToHomePage} className="pointer"/>
           <div className="line"></div>
           <h4>
             Virtual Class
