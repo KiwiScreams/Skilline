0e6da7ebc11552ced674608f8bdda09108597666
KiwiScreams <screamskiwi3@gmail.com>
2024-09-24 21:24:57 +0400

    imports

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,10 @@
 import "./Layout.css";
 import pauseImage from "../../assets/images/pause.png";
-
+import girlImage from "../../assets/images/lovely-teenage-girl-with-curly-hair-posing-yellow-tshirt-min 1.png";
+import img1 from "../../assets/images/Group 7.png";
+import img2 from "../../assets/images/Group 9.png";
+import img3 from "../../assets/images/Group 10.png";
+import img4 from "../../assets/images/Group 13.png";
 const Layout = () => {
   return (
     <>
