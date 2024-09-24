efa10e48d21f0bac98e8ee12e54eccd1a3ef453c
KiwiScreams <screamskiwi3@gmail.com>
2024-09-24 23:21:51 +0400

    companies component

--- src/components/companies/Companies.jsx
+++ src/components/companies/Companies.jsx
@@ -1,14 +1,26 @@
 import "./Companies.css";
 import google from "../../assets/images/google.png";
 import netflix from "../../assets/images/netflix.png";
-import airhnb from "../../assets/images/airhnb.png";
+import airbnb from "../../assets/images/airbnb.png";
 import amazon from "../../assets/images/amazon.png";
 import facebook from "../../assets/images/facebook.png";
 import grab from "../../assets/images/grab.png";
 const Companies = () => {
-    return ( 
-        <></>
-     );
-}
- 
+  return (
+    <>
+      <section className="companies-section">
+        <p>Trusted by 5,000+ Companies Worldwide</p>
+        <div className="companies">
+            <img src={google} alt="" />
+            <img src={netflix} alt="" />
+            <img src={airbnb} alt="" />
+            <img src={amazon} alt="" />
+            <img src={facebook} alt="" />
+            <img src={grab} alt="" />
+        </div>
+      </section>
+    </>
+  );
+};
+
 export default Companies;
