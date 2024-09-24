4d871078d2cb5ed7c6706095e35619c9bdbcc3be
KiwiScreams <screamskiwi3@gmail.com>
2024-09-24 23:16:55 +0400

    imports

--- src/components/companies/Companies.jsx
+++ src/components/companies/Companies.jsx
@@ -1,4 +1,10 @@
 import "./Companies.css";
+import google from "../../assets/images/google.png";
+import netflix from "../../assets/images/netflix.png";
+import airhnb from "../../assets/images/airhnb.png";
+import amazon from "../../assets/images/amazon.png";
+import facebook from "../../assets/images/facebook.png";
+import grab from "../../assets/images/grab.png";
 const Companies = () => {
     return ( 
         <></>
