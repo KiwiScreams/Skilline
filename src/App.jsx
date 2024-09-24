035e4ab7735a00b6f677ac35b2b901146fb92a2e
KiwiScreams <screamskiwi3@gmail.com>
2024-09-24 22:29:42 +0400

    not found page style

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
       </Routes>
       <Footer />
     </>
