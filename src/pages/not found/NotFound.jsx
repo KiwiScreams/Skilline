584d8932f0e1350a2b3bd7bc3b4fefab4a04010e
KiwiScreams <screamskiwi3@gmail.com>
2024-09-24 22:42:13 +0400

    handlenavigate to home

--- src/pages/not found/NotFound.jsx
+++ src/pages/not found/NotFound.jsx
@@ -1,6 +1,12 @@
 import "./NotFound.css";
 import notFoundImage from "../../assets/images/404-error.png";
+import { useNavigate } from "react-router-dom";
 const NotFound = () => {
+  const navigate = useNavigate();
+  const handleNavigateToHome = () => 
+  {
+    navigate("/");
+  }
   return (
     <>
       <section className="notfound-section">
