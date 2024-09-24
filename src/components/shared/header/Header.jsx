49b0ae8273903f598d85c458b875bcd41bb043e1
KiwiScreams <screamskiwi3@gmail.com>
2024-09-24 19:32:21 +0400

    header

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,12 +1,24 @@
 import "./Header.css";
+import logo from "../../../assets/images/dark logo.svg";
+import { Link } from "react-router-dom";
 const Header = () => {
-    return ( 
-        <>
-        <header>
-            
-        </header>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <header>
+        <h1>
+          <img src={logo} alt="Skilline" />
+        </h1>
+        <nav>
+            <Link to="/">Home</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Blog</Link>
+            <Link to="/">About Us</Link>
+            <Link to="/" className="login btn">Login</Link>
+            <Link to="/" className="signup btn">Sign Up</Link>
+        </nav>
+      </header>
+    </>
+  );
+};
+
 export default Header;
