96f0efe009e9077307afdd8b8be84942004cf25f
KiwiScreams <screamskiwi3@gmail.com>
2024-09-24 23:32:27 +0400

    companies array

--- src/components/companies/Companies.jsx
+++ src/components/companies/Companies.jsx
@@ -6,17 +6,25 @@ import amazon from "../../assets/images/amazon.png";
 import facebook from "../../assets/images/facebook.png";
 import grab from "../../assets/images/grab.png";
 const Companies = () => {
+  <div className="companies">
+    <img src={google} alt="Google logo" />
+    <img src={netflix} alt="Netflix logo" />
+    <img src={airbnb} alt="Airbnb logo" />
+    <img src={amazon} alt="Amazon logo" />
+    <img src={facebook} alt="Facebook logo" />
+    <img src={grab} alt="Grab logo" />
+  </div>;
   return (
     <>
       <section className="companies-section">
         <p>Trusted by 5,000+ Companies Worldwide</p>
         <div className="companies">
-            <img src={google} alt="Google logo" />
-            <img src={netflix} alt="Netflix logo" />
-            <img src={airbnb} alt="Airbnb logo" />
-            <img src={amazon} alt="Amazon logo" />
-            <img src={facebook} alt="Facebook logo" />
-            <img src={grab} alt="Grab logo" />
+          <img src={google} alt="Google logo" />
+          <img src={netflix} alt="Netflix logo" />
+          <img src={airbnb} alt="Airbnb logo" />
+          <img src={amazon} alt="Amazon logo" />
+          <img src={facebook} alt="Facebook logo" />
+          <img src={grab} alt="Grab logo" />
         </div>
       </section>
     </>
