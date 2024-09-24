2ae89c8b3c6e7b6861b706201c9253c4b3740d92
KiwiScreams <screamskiwi3@gmail.com>
2024-09-24 19:19:12 +0400

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/shared/header/Header";
+import Home from "./pages/home/Home";
+import Footer from "./components/shared/footer/Footer";
 
+function App() {
   return (
     <>
-      
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
