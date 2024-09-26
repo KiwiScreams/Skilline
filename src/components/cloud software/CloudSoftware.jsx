e9c8c431288f8ef082887c5a15f0072aa142a9f0
KiwiScreams <screamskiwi3@gmail.com>
2024-09-26 18:03:33 +0400

    software component

--- src/components/cloud software/CloudSoftware.jsx
+++ src/components/cloud software/CloudSoftware.jsx
@@ -1,8 +1,18 @@
 import "./CloudSoftware.css";
 const CloudSoftware = () => {
-    return ( 
-        <>cl</>
-     );
-}
- 
+  return (
+    <>
+      <section className="software-section">
+        <h3>
+          All-In-One <span className="orange-txt">Cloud Software.</span>
+        </h3>
+        <p>
+          Skilline is one powerful online software suite that combines all the
+          tools needed to run a successful school or office.
+        </p>
+      </section>
+    </>
+  );
+};
+
 export default CloudSoftware;
