48e04161394403088301bceab4897bf88561f07b
KiwiScreams <screamskiwi3@gmail.com>
2024-09-26 18:09:38 +0400

    create software info array

--- src/components/cloud software/CloudSoftware.jsx
+++ src/components/cloud software/CloudSoftware.jsx
@@ -2,7 +2,26 @@ import "./CloudSoftware.css";
 import img1 from "../../assets/images/Group 5396.svg";
 import img2 from "../../assets/images/Group 5397.svg";
 import img3 from "../../assets/images/Group 5398.svg";
-
+const softwareInfo = [
+  {
+    img: img1,
+    title: "Online Billing, Invoicing, & Contracts",
+    description:
+      "Simple and secure control of your organization's financial and legal transactions. Send customized invoices and contracts",
+  },
+  {
+    img: img2,
+    title: "Easy Scheduling & Attendance Tracking",
+    description:
+      "Schedule and reserve classrooms at one campus or multiple campuses. Keep detailed records of student attendance",
+  },
+  {
+    img: img3,
+    title: "Customer Tracking",
+    description:
+      "Automate and track emails to individuals or groups. Skilline's built-in system helps organize your organization",
+  },
+];
 const CloudSoftware = () => {
   return (
     <>
