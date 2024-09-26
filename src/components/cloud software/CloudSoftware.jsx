9d1948d420e693b72e57654972544518ac7603c5
KiwiScreams <screamskiwi3@gmail.com>
2024-09-26 18:08:39 +0400

    software info box

--- src/components/cloud software/CloudSoftware.jsx
+++ src/components/cloud software/CloudSoftware.jsx
@@ -14,6 +14,32 @@ const CloudSoftware = () => {
           Skilline is one powerful online software suite that combines all the
           tools needed to run a successful school or office.
         </p>
+        <div className="boxes">
+          <div className="box">
+            <img src={img1} alt="" />
+            <h4>Online Billing, Invoicing, & Contracts</h4>
+            <p>
+              Simple and secure control of your organization's financial and
+              legal transactions. Send customized invoices and contracts
+            </p>
+          </div>
+          <div className="box">
+            <img src={img2} alt="" />
+            <h4>Easy Scheduling & Attendance Tracking</h4>
+            <p>
+              Schedule and reserve classrooms at one campus or multiple
+              campuses. Keep detailed records of student attendance
+            </p>
+          </div>
+          <div className="box">
+            <img src={img3} alt="" />
+            <h4>Customer Tracking</h4>
+            <p>
+              Automate and track emails to individuals or groups. Skilline's
+              built-in system helps organize your organization
+            </p>
+          </div>
+        </div>
       </section>
     </>
   );
